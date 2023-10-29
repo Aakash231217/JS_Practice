a1f9ab87f8222c18e6e7c316ee207828e7a108d3
Aakash <rktaakash@gmail.com>
2023-10-30 01:06:49 +0530

    first commit

--- getterAndSetter.js
+++ getterAndSetter.js
@@ -7,15 +7,18 @@ class Person {
     get fullName(){
         return `${this.firstName} ${this.lastName}`
     }
-    setName(firstName, lastName){
+    set fullName(fullName){
+        const[firstName,lastName]= fullName.split(" ");
         this.firstName=firstName;
         this.lastName=lastName;
     }
 }
 
 const person1 = new Person("Aakash","singh",19)
-console.log(person1.fullName);
+//console.log(person1.fullName);
 
-person1.setName("Shristi","singh");
-console.log(person1.fullName);
+//person1.setName("Shristi","singh");
+//console.log(person1.fullName);
 // now by using get we can use it as property
+person1.fullName="hi ak";
+console.log(person1);
