aa2064e218b3bca314f8bd7c0bda98b3af91d711
Aakash <rktaakash@gmail.com>
2023-10-31 02:31:14 +0530

    first commit

---
+++ javascriptworks.js
@@ -0,0 +1,14 @@
+console.log(this);
+console.log(window);
+console.log(firstName);
+var firstName= "Aakash";
+console.log(firstName);
+//1. compile
+//i - Tokenizing/Lexing
+//ii- Parsing
+//iii- Code Generation
+
+//2. Code Execute
+//Global Execution Context
+// Creation Phase -> window {}, firstName: undefined, this:window
+// js in single threaded
